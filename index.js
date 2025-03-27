849de24a884b114f51488cf99ab1b7b6075cdfb2
Vaibhav Mujumdar <vaibhavmujumdar8@gmail.com>
2025-03-27 09:27:09 +0530

    Add dotenv support and update .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import express from "express";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.get('/',(req,res) => {
     res.send('Hello VOiD');
